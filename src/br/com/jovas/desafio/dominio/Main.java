087582768ed4ddde905a5b3ecfcfda5b6549f8ba
jovasdevs <jovasdevs@gmail.com>
2022-01-25 20:19:26 -0300

    Final

--- src/br/com/jovas/desafio/dominio/Main.java
+++ src/br/com/jovas/desafio/dominio/Main.java
@@ -20,9 +20,39 @@ public class Main {
         mentoria.setDescricao("Desafio Programação Orientada Objeto");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
-        
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Básico");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJovas = new Dev();
+        devJovas.setNome("Jovas");
+        devJovas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Jovas: " + devJovas.getConteudosInscritos());
+        devJovas.progredir();
+        System.out.println("=============================================================================================================");
+        System.out.println("Conteúdos Inscritos Jovas: " + devJovas.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Jovas: " + devJovas.getConteudosConcluidos());
+        System.out.println("XP: " + devJovas.calcularTotalXp());
+
+        System.out.println("===============");
+
+        Dev devGigi = new Dev();
+        devGigi.setNome("Gigi");
+        devGigi.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Gigi: " + devGigi.getConteudosInscritos());
+        devGigi.progredir();
+        System.out.println("==============================================================================================================");
+        System.out.println("Conteúdos Inscritos Gigi: " + devGigi.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Gigi: " + devGigi.getConteudosConcluidos());
+        System.out.println("XP: " + devGigi.calcularTotalXp());
+
     }
+
 }
